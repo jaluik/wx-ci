d203dc6da89137399ebc2699bc5f19f5221a3624
jaluik <cqzhengmouren@gmail.com>
2020-11-21 19:28:55 +0800

    feat: 更新配置了

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,28 @@
 import { preserveShebangs } from 'rollup-plugin-preserve-shebangs';
+import typescript from 'rollup-plugin-typescript';
+import json from '@rollup/plugin-json';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
+const key = Object.keys(require('./package.json').dependencies);
+
 export default {
-  input: 'bin/wx-ci.js',
+  input: 'src/index.ts',
   output: {
-    file: 'dist/wxCi.js',
+    file: 'bin/wx-ci.js',
     format: 'cjs',
   },
-  plugins: [preserveShebangs(), resolve(), commonjs()],
+  plugins: [
+    preserveShebangs(),
+    resolve({
+      preferBuiltins: true,
+    }),
+    commonjs(),
+    json(),
+    typescript({
+      exclude: 'node_modules/**',
+      typescript: require('typescript'),
+    }),
+  ],
+  external: [...key, /package\.json/],
 };
